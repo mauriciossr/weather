1776ec7ebe20f5a571e2516fa55c4478fba49359
Mauricio S. S. Reis <50428134+mauriciossr@users.noreply.github.com>
2023-06-25 17:28:43 -0300

    Criado variaveis para conexão com o banco e migration

---
+++ src/migration/1687724510358-create_table_favorite_city.ts
@@ -0,0 +1,37 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class CreateTableFavoriteCity1687724510358
+  implements MigrationInterface
+{
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    queryRunner.query(`
+            CREATE TABLE public.favoriteCity (
+                id integer NOT NULL,
+                cityId character varying NOT NULL,
+                cityName character varying NOT NULL,
+                userId integer varying NOT NULL,
+                createdAt timestamp without time zone DEFAULT now() NOT NULL,
+                updatedAt timestamp without time zone DEFAULT now() NOT NULL,
+                primary key (id)
+            );
+
+            CREATE SEQUENCE public.favoriteCity_id_seq
+                AS integer
+                START WITH 1
+                INCREMENT BY 1
+                NO MINVALUE
+                NO MAXVALUE
+                CACHE 1;
+
+            ALTER SEQUENCE public.favoriteCity_id_seq OWNED BY public.favoriteCity.id;
+
+            ALTER TABLE ONLY  public.favoriteCity ALTER COLUMN id SET DEFAULT nextval('public.favoriteCity_id_seq'::regclass);
+        `);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    queryRunner.query(`
+            drop table public.favoriteCity;
+        `);
+  }
+}
