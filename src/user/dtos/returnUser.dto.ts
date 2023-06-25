403d8608f7a358f9d22b502c19ce590ab655e789
Mauricio S. S. Reis <50428134+mauriciossr@users.noreply.github.com>
2023-06-25 19:41:05 -0300

    Ajustes no docker

---
+++ src/user/dtos/returnUser.dto.ts
@@ -0,0 +1,15 @@
+import { UserEntity } from '../interfaces/user.entity';
+
+export class ReturnUserDto {
+  id: number;
+  name: string;
+  email: string;
+  phone: string;
+
+  constructor(userEntity: UserEntity) {
+    this.id = userEntity.id;
+    this.name = userEntity.name;
+    this.email = userEntity.email;
+    this.phone = userEntity.phone;
+  }
+}
